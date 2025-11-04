b5b079732369a0b9703f73df25397c4c7320c551
Jack Van Hecke <jackvanhecke@MacBook-Air-van-Jack.local>
2025-11-04 09:54:55 +0100

    pictures

---
+++ Backend/recall/src/main/java/com/example/models/Pictures.java
@@ -0,0 +1,49 @@
+package com.example.models;
+import java.util.List;
+
+public class Pictures {
+    private int id;
+    private String url;
+    private Event event;
+    private String hashtags;
+
+
+
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+
+    }
+    public Event getEvent() {
+        return event;
+    }
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+    public String getHashtags() {
+        return hashtags;
+    }
+    public void setHashtags(String hashtags) {
+        this.hashtags = hashtags;
+    }
+    
+    public List<String> getHashtagsList() {
+        return List.of(hashtags.split(","));
+    }
+
+    public void setHashtagsList(List<String> hashtagsList) {
+        this.hashtags = String.join(",", hashtagsList);
+    }
+
+}
+
+

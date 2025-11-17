a1334effad9d6ba7dee5c8b5169e2f2136935a51
Austyne <augustineasimhi1@gmail.com>
2025-11-17 15:55:12 +0100

    events

--- Backend/recall/src/main/java/com/example/controller/EventController.java
+++ Backend/recall/src/main/java/com/example/controller/EventController.java
@@ -9,6 +9,9 @@ import com.example.service.EventService;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -24,6 +27,14 @@ public class EventController {
     public List<Event> getAllEvents() {
         return eventService.getAllEvents();
     }
+
+    @PostMapping("")
+    public void createEvent(@RequestBody Event entityData) {
+
+        eventService.createEvent(entityData);
+        
+    }
+    
     
 
 

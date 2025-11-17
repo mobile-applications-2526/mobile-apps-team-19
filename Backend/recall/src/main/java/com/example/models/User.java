1a2900c034f9bdddf6fc70247a65a997653a88b5
Meast04 <leenen.maxime@gmail.com>
2025-11-17 13:25:24 +0100

    user fixed without id in constr

--- Backend/recall/src/main/java/com/example/models/User.java
+++ Backend/recall/src/main/java/com/example/models/User.java
@@ -17,12 +17,10 @@ public class User {
     private String email;
     private String password;
 
-
     public User() {
     }
 
-    public User(Long id, String username, String email, String firstName, String lastName, String password) {
-         this.id = id;
+    public User(String username, String email, String firstName, String lastName, String password) {
         setUsername(username);
         setEmail(email);
         setFirstName(firstName);
@@ -49,24 +47,29 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getFirstName() {
         return firstName;
     }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
 
-    
 }
